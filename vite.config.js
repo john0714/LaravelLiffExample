2a79611cb5e6f89cd4b97e417011d687bf3aa0a8
Kim <john0714@naver.com>
2022-08-29 23:33:51 +0900

    add eslint

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    plugins: [
-        // basicSsl(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        // https: true,
-        hmr: {
-            host: 'localhost',
-        },
-        watch: {
-            usePolling: true
-        }
+  plugins: [
+    // basicSsl(),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    // https: true,
+    hmr: {
+      host: 'localhost',
+    },
+    watch: {
+      usePolling: true
     }
+  }
 });
